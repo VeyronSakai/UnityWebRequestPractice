1304ea52d226383e10a1d13d7c833c480f20c670
VeyronSakai <yuukisakai123@gmail.com>
2021-01-23 22:51:37 +0900

    commit

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -4,19 +4,40 @@ using UnityEngine.Networking;
 
 public class MainController : MonoBehaviour
 {
-    private const string URL = "http://localhost:8080/hoge";
+    private const string TruthUri = "http://localhost:8080/truth";
+    private const string LieUri = "http://localhost:8080/lie";
+    private const string StartUri = "https://talpidae-backend.herokuapp.com/start";
 
     private async void Start()
     {
-        await RequestAsync();
+        // await RequestAsync();
+        await RequestStartAsync();
     }
 
     private static async UniTask RequestAsync()
     {
-        var webRequest = UnityWebRequest.Get(URL);
+        var webRequest = UnityWebRequest.Get(LieUri);
 
         await webRequest.SendWebRequest();
 
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+        {
+            //通信失敗
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            //通信成功
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+    }
+
+    private static async UniTask RequestStartAsync()
+    {
+        var webRequest = UnityWebRequest.Get(StartUri);
+
+        await webRequest.SendWebRequest();
+        
         if (webRequest.result == UnityWebRequest.Result.ConnectionError)
         {
             //通信失敗
